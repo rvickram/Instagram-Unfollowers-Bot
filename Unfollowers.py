0c4f851ea9ec9a27cdce688ae7fb89aaf5a8b263
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-01-20 18:48:42 -0500

    Properly check for, and load the chrome web driver.

--- Unfollowers.py
+++ Unfollowers.py
@@ -1,8 +1,15 @@
-import selenium as webdriver
+# Instagram Unfollowers bot by Ryan Vickramasinghe
+
+import selenium.webdriver as webdriver
+import os.path as path
 
 class UnfollowersBot:
     def __init__(self):
-        self.driver = webdriver.Chrome('./chromedriver')
-        self.driver.get("https://instagram.com")
+        print('\n\n---- Initializing Instagram Unfollowers Bot ----\n\n')
+        if(path.exists('./chromedriver')):
+            self.driver = webdriver.Chrome('./chromedriver')
+            self.driver.get("https://instagram.com")
+        else:
+            print('No chrome driver detected in path!')
 
 UnfollowersBot()
