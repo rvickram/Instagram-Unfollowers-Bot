3feb22e9a917ea5c8e0e7580e555e29984bbdbb4
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-01-20 18:38:13 -0500

    Load the chrome web driver.

--- Unfollowers.py
+++ Unfollowers.py
@@ -2,7 +2,7 @@ import selenium as webdriver
 
 class UnfollowersBot:
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome('./chromedriver')
         self.driver.get("https://instagram.com")
 
 UnfollowersBot()
