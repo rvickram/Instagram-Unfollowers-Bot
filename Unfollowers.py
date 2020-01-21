385a1eef81d233930c4e50a6c6e288dc46d2f637
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-01-21 10:08:38 -0500

    Able to login to account.
    Still need to deal with two-factor authentication.

--- Unfollowers.py
+++ Unfollowers.py
@@ -1,15 +1,42 @@
 # Instagram Unfollowers bot by Ryan Vickramasinghe
 
-import selenium.webdriver as webdriver
-import os.path as path
+from selenium import webdriver
+from os import path
+from getpass import getpass
+from time import sleep
 
 class UnfollowersBot:
-    def __init__(self):
-        print('\n\n---- Initializing Instagram Unfollowers Bot ----\n\n')
+    def __init__(self, username, password):
+        print('\nAttempting to login...')
+
+        # check for chromedriver
         if(path.exists('./chromedriver')):
             self.driver = webdriver.Chrome('./chromedriver')
             self.driver.get("https://instagram.com")
-        else:
-            print('No chrome driver detected in path!')
+            sleep(2)
 
-UnfollowersBot()
+            # attempt to login using username, password
+            self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]")\
+                .click()
+            sleep(2)
+            self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
+                .send_keys(username)
+            self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
+                .send_keys(password)
+            self.driver.find_element_by_xpath('//button[@type="submit"]')\
+                .click()
+            # TODO: Check for two-factor authentication
+            self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+                .click()
+            sleep(2)
+            print('Successfully logged in.')
+        else:
+            print('ERROR: No chrome driver detected in path!')
+
+print('\n\n---- Welcome to Instagram Unfollowers Bot ----\n\n')
+# get login details from console (user must enter)
+username = input('Username: ')
+password = getpass('Password: ')
+
+# initialize the bot (login)
+bot = UnfollowersBot(username, password)
